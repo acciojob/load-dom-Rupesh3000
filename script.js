6f9e14422d9cca01b5260756064a6f2638cf5fb3
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2025-08-21 05:27:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", function () {
-  // Create a new paragraph element
-  const newParagraph = document.createElement("p");
-
-  // Create a text node with the desired string
-  const textContent = document.createTextNode("DOM load success);
-
-  // Append the text node to the paragraph element
-  newParagraph.appendChild(textContent);
-
-  // Append the new paragraph element to the body of the document
-  document.body.appendChild(newParagraph);
+document.body.innerHTML = "DOM load success"  
 });
